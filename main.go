66af0bdb88c43300a8ef111d7f65e2345c887059
Ramon Vermeulen <ramonvermeulen98@gmail.com>
2025-09-05 20:16:13 +0200

    feat: CH1L4 - Newlines

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"io"
 	"os"
+	"strings"
 )
 
 func main() {
@@ -14,10 +15,26 @@ func main() {
 
 	content := make([]byte, 8)
 	n, err := msg.Read(content)
+	var line string
+
 	for err == nil {
-		fmt.Printf("read: %s\n", content[:n])
+		parts := strings.Split(string(content[:n]), "\n")
+		if len(parts) > 1 {
+			for i := range len(parts) {
+				if i == len(parts)-1 {
+					line += parts[i]
+					break
+				}
+				line += parts[i]
+				fmt.Printf("read: %s\n", line)
+				line = ""
+			}
+		} else {
+			line += parts[0]
+		}
 		n, err = msg.Read(content)
 	}
+
 	if err == io.EOF {
 		os.Exit(0)
 	} else {
