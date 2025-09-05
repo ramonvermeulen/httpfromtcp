20e185f77760148340246f8221a9d84f6f295c23
Ramon Vermeulen <ramonvermeulen98@gmail.com>
2025-09-05 21:00:56 +0200

    feat: CH1L5 - Channel Refactor

--- main.go
+++ main.go
@@ -7,37 +7,49 @@ import (
 	"strings"
 )
 
+func getLinesChannel(f io.ReadCloser) <-chan string {
+	c := make(chan string, 1)
+	content := make([]byte, 8)
+
+	go func() {
+		defer close(c)
+		defer f.Close()
+		var line string
+		for {
+			n, err := f.Read(content)
+			content := content[:n]
+			if err != nil {
+				break
+			}
+
+			parts := strings.Split(string(content), "\n")
+			if len(parts) > 1 {
+				for i := range len(parts) {
+					if i == len(parts)-1 {
+						line += parts[i]
+						break
+					}
+					line += parts[i]
+					c <- line
+					line = ""
+				}
+			} else {
+				line += parts[0]
+			}
+		}
+	}()
+
+	return c
+}
+
 func main() {
 	msg, err := os.Open("messages.txt")
 	if err != nil {
 		panic(fmt.Errorf("error occurred while opening file: %w", err))
 	}
 
-	content := make([]byte, 8)
-	n, err := msg.Read(content)
-	var line string
-
-	for err == nil {
-		parts := strings.Split(string(content[:n]), "\n")
-		if len(parts) > 1 {
-			for i := range len(parts) {
-				if i == len(parts)-1 {
-					line += parts[i]
-					break
-				}
-				line += parts[i]
-				fmt.Printf("read: %s\n", line)
-				line = ""
-			}
-		} else {
-			line += parts[0]
-		}
-		n, err = msg.Read(content)
-	}
-
-	if err == io.EOF {
-		os.Exit(0)
-	} else {
-		panic(fmt.Errorf("error occurred while reading file: %w", err))
+	c := getLinesChannel(msg)
+	for line := range c {
+		fmt.Printf("read: %s\n", line)
 	}
 }
