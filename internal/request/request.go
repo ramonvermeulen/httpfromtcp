2b31afec2d2c8591b5474a50f8115f0ef347f7b9
Ramon Vermeulen <ramonvermeulen98@gmail.com>
2025-09-07 16:32:33 +0200

    feat: CH4L3 - Parsing a Stream (+ additionaly switches to byte processing)

--- internal/request/request.go
+++ internal/request/request.go
@@ -1,10 +1,36 @@
 package request
 
 import (
+	"bytes"
 	"fmt"
 	"io"
 	"slices"
-	"strings"
+)
+
+var HTTPMethods = [][]byte{
+	[]byte("GET"),
+	[]byte("HEAD"),
+	[]byte("POST"),
+	[]byte("PUT"),
+	[]byte("DELETE"),
+	[]byte("CONNECT"),
+	[]byte("OPTIONS"),
+	[]byte("TRACE"),
+}
+
+var (
+	ErrMalformedRequestLine   = fmt.Errorf("malformed request line")
+	ErrUnsupportedHttpVersion = fmt.Errorf("unsupported HTTP version")
+	ErrUnsupportedHttpMethod  = fmt.Errorf("unsupported HTTP method")
+	ErrParsingInDoneState     = fmt.Errorf("attempted to parse request in done state")
+	LineSeparator             = []byte("\r\n")
+)
+
+type requestState int
+
+const (
+	Initialized requestState = iota
+	Done                     = iota
 )
 
 type RequestLine struct {
@@ -15,69 +41,84 @@ type RequestLine struct {
 
 type Request struct {
 	RequestLine RequestLine
+	state       requestState
 }
 
-var HTTPMethods = []string{
-	"GET",
-	"HEAD",
-	"POST",
-	"PUT",
-	"DELETE",
-	"CONNECT",
-	"OPTIONS",
-	"TRACE",
+func (r *Request) parse(data []byte) (int, error) {
+outer:
+	for {
+		switch r.state {
+		case Initialized:
+			bytesProcessed, requestLine, err := parseRequestLine(data)
+			if err != nil {
+				return 0, err
+			}
+			if bytesProcessed == 0 {
+				break outer
+			}
+
+			r.RequestLine = *requestLine
+			r.state = Done
+			return len(data), nil
+		case Done:
+			return 0, ErrParsingInDoneState
+		}
+	}
+	return 0, nil
 }
 
-var (
-	ERR_MALFORMED_REQUEST_LINE   = fmt.Errorf("malformed request line")
-	ERR_UNSUPPORTED_HTTP_VERSION = fmt.Errorf("unsupported HTTP version")
-	ERR_UNSUPOERTED_HTTP_METHOD  = fmt.Errorf("unsupported HTTP method")
-)
-
-func parseRequestLine(line string) (*RequestLine, error) {
-	parts := strings.Split(line, " ")
-	if len(parts) != 3 {
-		return nil, ERR_MALFORMED_REQUEST_LINE
+func parseRequestLine(line []byte) (int, *RequestLine, error) {
+	if !bytes.Contains(line, LineSeparator) {
+		return 0, nil, nil
 	}
 
-	versionParts := strings.Split(parts[2], "/")
+	line = bytes.SplitN(line, LineSeparator, 2)[0]
+	parts := bytes.Split(line, []byte(" "))
+	if len(parts) != 3 {
+		return 0, nil, ErrMalformedRequestLine
+	}
+
+	versionParts := bytes.Split(parts[2], []byte("/"))
 	requestTarget := parts[1]
 	method := parts[0]
 
-	if len(versionParts) != 2 || versionParts[0] != "HTTP" || versionParts[1] != "1.1" {
-		return nil, ERR_UNSUPPORTED_HTTP_VERSION
+	if len(versionParts) != 2 || !bytes.Equal(versionParts[0], []byte("HTTP")) || !bytes.Equal(versionParts[1], []byte("1.1")) {
+		return 0, nil, ErrUnsupportedHttpVersion
 	}
 
-	match := slices.Contains(HTTPMethods, method)
-	if !match {
-		return nil, ERR_UNSUPOERTED_HTTP_METHOD
+	if !slices.ContainsFunc(HTTPMethods, func(m []byte) bool {
+		return bytes.Equal(m, method)
+	}) {
+		return 0, nil, ErrUnsupportedHttpMethod
 	}
 
-	return &RequestLine{
-		HTTPVersion:   versionParts[1],
-		RequestTarget: requestTarget,
-		Method:        method,
+	return len([]byte(line)), &RequestLine{
+		HTTPVersion:   string(versionParts[1]),
+		RequestTarget: string(requestTarget),
+		Method:        string(method),
 	}, nil
 }
 
 func RequestFromReader(reader io.Reader) (*Request, error) {
-	data, err := io.ReadAll(reader)
-	if err != nil {
-		return nil, err
-	}
-	var requestLine *RequestLine
-	for i, line := range strings.Split(string(data), "\r\n") {
-		// for this exercise we only care about the request line
-		if i > 0 {
+	request := &Request{}
+	request.state = Initialized
+	buffer := make([]byte, 1024)
+	bufferLen := 0
+
+	for request.state != Done {
+		n, err := reader.Read(buffer[bufferLen:])
+		if err != nil {
 			break
 		}
-		requestLine, err = parseRequestLine(line)
+		bufferLen += n
+		bytesProcessed, err := request.parse(buffer[:bufferLen])
 		if err != nil {
 			return nil, err
 		}
+		if bytesProcessed > 0 {
+			buffer = buffer[bytesProcessed:]
+		}
 	}
 
-	return &Request{
-		RequestLine: *requestLine,
-	}, nil
+	return request, nil
 }
