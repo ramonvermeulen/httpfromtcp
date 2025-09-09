113aecf3742751ddee116d169577a50e79ff301a
Ramon Vermeulen <ramonvermeulen98@gmail.com>
2025-09-09 16:22:22 +0200

    feat: CH4L6 - Connect the Parsing

--- cmd/tcplistener/main.go
+++ cmd/tcplistener/main.go
@@ -2,52 +2,11 @@ package main
 
 import (
 	"fmt"
-	"io"
 	"log"
 	"net"
-	"strings"
+
+	"github.com/ramonvermeulen/httpfromtcp/internal/request"
 )
-
-func getLinesChannel(f io.ReadCloser) <-chan string {
-	c := make(chan string, 1)
-	content := make([]byte, 8)
-	var line string
-
-	go func() {
-		defer func() {
-			if line != "" {
-				c <- line
-			}
-			close(c)
-			fmt.Println("closed connection")
-			f.Close()
-		}()
-		for {
-			n, err := f.Read(content)
-			content := content[:n]
-			if err != nil {
-				break
-			}
-
-			parts := strings.Split(string(content), "\n")
-			if len(parts) > 1 {
-				for i := range len(parts) {
-					if i == len(parts)-1 {
-						line += parts[i]
-						break
-					}
-					line += parts[i]
-					c <- line
-					line = ""
-				}
-			} else {
-				line += parts[0]
-			}
-		}
-	}()
-
-	return c
-}
 
 func main() {
 	lis, err := net.Listen("tcp", ":42069")
@@ -63,9 +22,13 @@ func main() {
 
 		fmt.Println("accepted connection")
 
-		c := getLinesChannel(conn)
-		for line := range c {
-			fmt.Printf("read: %s\n", line)
+		rl, err := request.RequestFromReader(conn)
+		if err != nil {
+			log.Fatal("error processing request")
 		}
+		fmt.Println("Request line:")
+		fmt.Printf("- Method: %+v\n", rl.RequestLine.Method)
+		fmt.Printf("- Target: %+v\n", rl.RequestLine.RequestTarget)
+		fmt.Printf("- Version: %+v\n", rl.RequestLine.HTTPVersion)
 	}
 }
